0a0841d3380a9a9e9930944e3515e22b4d24c999
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-01-16 17:06:05 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,47 +1,8 @@
 #include <taihen.h>
 #include <psp2/paf.h>
-#include <psp2/kernel/sysmem.h>
-
-#include <libpng16/png.h>
-
-#include <libk/string.h>
-
-int __errno = 0;
-void abort(void) {
-	__builtin_trap();
-}
-
-void *malloc(size_t sz) {
-	return sce_paf_private_malloc(sz);
-}
-
-void free(void *p) {
-	sce_paf_private_free(p);
-}
-
-//////
-
-tai_hook_ref_t watermark_hook, encode_hook, encode_type2_hook, reg_hook;
-
-int get_key_int(const char *dir, const char *file, int *out) {
-	if (strcmp(dir, "/CONFIG/PHOTO") == 0 && strcmp(file, "debug_screenshot") == 0) {
-		*out = 1;
-		return 0;
-	}
-
-	return TAI_CONTINUE(int, reg_hook, dir, file, out);
-}
-
-int place_watermark_hook() {
-	return 0;
-}
-
-int encode_screenshot(void **ss_arg1, unsigned unk) {
-	// set format to 2 = "raw" but we will hook and change that later
-	((int*)(*ss_arg1))[8/4] = 2;
-
-	return TAI_CONTINUE(int, encode_hook, ss_arg1, unk);
-}
+#include <psp2/kernel/modulemgr.h>
+#include <psp2/kernel/clib.h>
+#include <psp2/registrymgr.h>
 
 typedef struct picture_t picture_t;
 
@@ -97,148 +58,272 @@ typedef struct {
 
 size_t g_png_size;
 
-void write_func(png_structp png_ptr, png_bytep data, png_size_t length) {
-	g_png_size += length;
-	actual_encode_args_t *args = png_get_io_ptr(png_ptr);
-	args->encode->vptr->append(args->encode, data, length);
-}
-
 enum {
 	ENCODE_ERROR1 = 0x80103001,
 	ENCODE_ERROR = 0x80103002,
 };
 
-int encode_type2(actual_encode_args_t *args) {
-	int ret = 0;
-	unsigned *pixels = NULL;
-	png_structp png_ptr = NULL;
-	png_infop info_ptr = NULL;
-	dimensions_t wh = {0};
+#define HookImport(module_name, library_nid, func_nid, func_name) ({ \
+	taiHookFunctionImport(&func_name ## _ref, module_name, library_nid, func_nid, func_name ## _patch); \
+})
 
-	encode_t *encode = args->encode;
-	picture_t *picture = args->picture;
+#define HookOffset(modid, offset, thumb, func_name) ({ \
+	taiHookFunctionOffset(&func_name ## _ref, modid, 0, offset, thumb, func_name ## _patch); \
+})
 
-	if (!encode || !encode->vptr->is_buffer_init(encode) || !picture || !picture->vptr->get_type(picture))
-		return ENCODE_ERROR;
+int hex_dump(const void *addr, int size){
 
-	g_png_size = 0;
-	picture->vptr->get_dimensions(&wh, picture);
+	addr = (const void *)(((uint32_t)addr) & ~0xF);
 
-	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-	if (!png_ptr)
-		goto cleanup;
+	size = (size + 0xF) & ~0xF;
 
-	info_ptr = png_create_info_struct(png_ptr);
-	if (!info_ptr)
-		goto cleanup;
+	for(int i=0;i<size;i+=0x10){
+		sceClibPrintf("0x%08X : %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X\n", (addr), ((char *)addr)[0x0], ((char *)addr)[0x1], ((char *)addr)[0x2], ((char *)addr)[0x3], ((char *)addr)[0x4], ((char *)addr)[0x5], ((char *)addr)[0x6], ((char *)addr)[0x7], ((char *)addr)[0x8], ((char *)addr)[0x9], ((char *)addr)[0xA], ((char *)addr)[0xB], ((char *)addr)[0xC], ((char *)addr)[0xD], ((char *)addr)[0xE], ((char *)addr)[0xF]);
 
-	png_set_check_for_invalid_index(png_ptr, 0);
+		addr += 0x10;
+	}
 
-	png_set_IHDR(png_ptr, info_ptr, wh.width, wh.height, 8,
-		PNG_COLOR_TYPE_RGBA,
-		PNG_INTERLACE_NONE,
-		PNG_COMPRESSION_TYPE_DEFAULT,
-		PNG_FILTER_TYPE_DEFAULT);
+	return 0;
+}
 
-	png_set_write_fn(png_ptr, args, write_func, NULL);
+/*
+ * BmpHeader by Princes of Sleeping
+ */
+typedef struct {
+	char magic[2];                 // "BM"
+	uint32_t full_file_size;       // 0x1FE08A : header size(0x8A) + image_size (960 * 544 * 4 : 0x1FE000)
+	uint16_t rev[2];               // 0, 0
+	uint32_t image_data_offset;    // 0xE + sub header size, 0x8A
+} __attribute__((packed)) BmpHeader_t; // 0xE
 
+typedef struct {
+	uint32_t sub_header_size;  // 0x28
+	uint32_t image_width_pix;
+	uint32_t image_height_pix;
+	uint16_t addr_0x1A_only_1; // 1
+	uint16_t bit;              // 32 or 24
+	uint32_t compression_type; // zero
+	uint32_t image_size;
+	char rsv1[0x10];
+} __attribute__((packed)) BmpSubHeader28_t; // 0x28
+
+typedef struct {
+	uint32_t sub_header_size;  // 0x7C
+	uint32_t image_width_pix;
+	uint32_t image_height_pix;
+	uint16_t addr_0x1A_only_1; // 1
+	uint16_t bit;              // 32
+	uint32_t compression_type; // 3
+	//0x22
+	uint32_t image_size;
+	uint32_t unk1;
+	uint32_t unk2;
+	uint32_t unk3;
+	uint32_t unk4;
+	//0x36 - color code ?
+	uint32_t unk5; // 0x00FF0000
+	uint32_t unk6; // 0x0000FF00
+	uint32_t unk7; // 0x000000FF
+	uint32_t unk8; // 0xFF000000
+	//0x46
+	uint32_t BGRs_magic;
+	char unk9[0x30];
+	uint32_t unk10;// 2 ?
+	char unk11[0xC];
+} __attribute__((packed)) BmpSubHeader7C_t; // 0x7C
+
+int get_system_screenshot_format(void){
+
+	int res;
+	int val = -1;
+
+	res = sceRegMgrGetKeyInt("/CONFIG/PHOTO", "debug_screenshot_format", &val);
+	if(res < 0)
+		return res;
+
+	if(val == 1)
+		return 1; // bmp
+
+	if(val == 0)
+		return 0; // jpg
+
+	return -1;
+}
+
+int encode_bmp(actual_encode_args_t *args)
+{
+	int res;
+	dimensions_t wh;
+
+	args->picture->vptr->get_dimensions(&wh, args->picture);
+
+	BmpHeader_t BmpHeader;
+	BmpSubHeader28_t BmpSubHeader28;
+
+	sceClibMemset(&BmpHeader, 0, sizeof(BmpHeader_t));
+	sceClibMemset(&BmpSubHeader28, 0, sizeof(BmpSubHeader28_t));
+
+	BmpHeader.magic[0] = 'B';
+	BmpHeader.magic[1] = 'M';
+	BmpHeader.full_file_size = sizeof(BmpHeader_t) + sizeof(BmpSubHeader28_t) + (wh.width * wh.height * 4);
+	BmpHeader.image_data_offset = sizeof(BmpHeader_t) + sizeof(BmpSubHeader28_t);
+
+	BmpSubHeader28.sub_header_size  = sizeof(BmpSubHeader28);
+	BmpSubHeader28.image_width_pix  = wh.width;
+	BmpSubHeader28.image_height_pix = wh.height;
+	BmpSubHeader28.addr_0x1A_only_1 = 1;
+	BmpSubHeader28.bit              = 32;
+	BmpSubHeader28.compression_type = 0;
+	BmpSubHeader28.image_size       = (wh.width * wh.height * 4);
+
+	res = sizeof(BmpHeader_t);
+	args->encode->vptr->append(args->encode, &BmpHeader, sizeof(BmpHeader_t));
+
+	res += sizeof(BmpSubHeader28_t);
+	args->encode->vptr->append(args->encode, &BmpSubHeader28, sizeof(BmpSubHeader28_t));
+
+	int *pixels;
 	pixels = sce_paf_private_malloc(wh.width * 4);
 
-	png_write_info(png_ptr, info_ptr);
+	uint32_t color32[4];
 
-	for (int i = 0; i < wh.height; ++i) {
+	for (int i = wh.height - 1; i >= 0 ; --i) {
+
 		// haven't reversed what this does, just copied from assembly
 		unk_obj_t *unk = args->field_8;
 		if (unk && unk->func && unk->func(unk->field_4)) {
-			ret = ENCODE_ERROR1;
+			res = ENCODE_ERROR1;
 			goto cleanup;
 		}
 
-		for (int j = 0; j < wh.width; ++j)
-			pixels[j] = picture->vptr->get_pixel(picture, j, i) | 0xFF000000u;
+		for (int j = wh.width - 1; j >= 0; --j){
 
-		png_write_row(png_ptr, (void*)pixels);
+			uint32_t color = args->picture->vptr->get_pixel(args->picture, j, i);
+
+			color32[0] = (color >> 0) & 0xFF;
+			color32[1] = (color >> 8) & 0xFF;
+			color32[2] = (color >> 16) & 0xFF;
+
+			pixels[j] = (color32[0] << 16) | (color32[1] << 8) | (color32[2] << 0) | 0xFF000000;
+		}
+
+		args->encode->vptr->append(args->encode, pixels, wh.width * 4);
+		res += (wh.width * 4);
 	}
-
-	png_write_end(png_ptr, info_ptr);
 
 cleanup:
-	if (png_ptr || info_ptr)
-		png_destroy_write_struct(&png_ptr, &info_ptr);
+	sce_paf_private_free(pixels);
 
-	if (pixels)
-		sce_paf_private_free(pixels);
-
-	if (ret < 0)
-		return ret;
-
-	return g_png_size;
+	return res;
 }
 
-int module_start() {
-	tai_module_info_t info = {0};
-	info.size = sizeof(info);
-	taiGetModuleInfo(TAI_MAIN_MODULE, &info);
+// disable watermark
+tai_hook_ref_t shell_add_watermark_ref;
+int shell_add_watermark_patch(int a1, int a2){
+	return 0;
+}
 
-	if (info.module_nid == 0x0552F692) { // 3.60 retail
-		// disable watermark
-		taiHookFunctionOffset(&watermark_hook, info.modid, 0, 0x247e00, 1, place_watermark_hook);
+// Replace type = 2 encoding with any implementation
+tai_hook_ref_t shell_encode_type2_ref;
+int shell_encode_type2_patch(actual_encode_args_t *args)
+{
+	int res;
 
-		// enable type=2 screenshot encoding
-		taiHookFunctionOffset(&encode_hook, info.modid, 0, 0x365f46, 1, encode_screenshot);
+	if (args->encode == NULL)
+		return ENCODE_ERROR;
 
-		// replace type=2 encoding with our png implementation
-		taiHookFunctionOffset(&encode_type2_hook, info.modid, 0, 0x36bd22, 1, encode_type2);
+	if (!args->encode->vptr->is_buffer_init(args->encode))
+		return ENCODE_ERROR;
 
-		// change branch for 0x34560004 (screenshot disable) to 0x34560003 (screenshot enable)
-		int value = 0x3b;
-		taiInjectData(info.modid, 0, 0x248840, &value, 2);
+	if (args->picture == NULL)
+		return ENCODE_ERROR;
 
-		// change extension from jpg to png
-		const char *path = "ur0:temp/screenshot/capture.png";
-		taiInjectData(info.modid, 0, 0x5148b8, path, strlen(path) + 1);
-	} else if (info.module_nid == 0xEAB89D5C) //3.60 Testkit
-	{
-		// disable watermark
-		taiHookFunctionOffset(&watermark_hook, info.modid, 0, 0x240234, 1, place_watermark_hook);
+	if (!args->picture->vptr->get_type(args->picture))
+		return ENCODE_ERROR;
 
-		// enable type=2 screenshot encoding
-		taiHookFunctionOffset(&encode_hook, info.modid, 0, 0x35c98e, 1, encode_screenshot);
 
-		// replace type=2 encoding with our png implementation
-		taiHookFunctionOffset(&encode_type2_hook, info.modid, 0, 0x36276a, 1, encode_type2);
-
-		// change branch for 0x34560004 (screenshot disable) to 0x34560003 (screenshot enable)
-		int value = 0x3b;
-		taiInjectData(info.modid, 0, 0x240C74, &value, 2);
-
-		// change extension from jpg to png
-		const char *path = "ur0:temp/screenshot/capture.png";
-		taiInjectData(info.modid, 0, 0x508B18, path, strlen(path) + 1);
-	}else if (info.module_nid == 0x5549BF1F ||
-		   info.module_nid == 0x34B4D82E ||
-		   info.module_nid == 0x12DAC0F3) { // 3.65/3.67/3.68 retail
-		// disable watermark
-		taiHookFunctionOffset(&watermark_hook, info.modid, 0, 0x247e9c, 1, place_watermark_hook);
-
-		// enable type=2 screenshot encoding
-		taiHookFunctionOffset(&encode_hook, info.modid, 0, 0x36638a, 1, encode_screenshot);
-
-		// replace type=2 encoding with our png implementation
-		taiHookFunctionOffset(&encode_type2_hook, info.modid, 0, 0x36c166, 1, encode_type2);
-
-		// change branch for 0x34560004 (screenshot disable) to 0x34560003 (screenshot enable)
-		int value = 0x3b;
-		taiInjectData(info.modid, 0, 0x2488dc, &value, 2);
-
-		// change extension from jpg to png
-		const char *path = "ur0:temp/screenshot/capture.png";
-		taiInjectData(info.modid, 0, 0x514df8, path, strlen(path) + 1);
+	res = get_system_screenshot_format();
+	if(res < 0){
+		res = encode_bmp(args);
+	}else if(res == 0){ // for devkit jpg
+		res = TAI_CONTINUE(int, shell_encode_type2_ref, args);
+	}else if(res == 1){
+		res = encode_bmp(args);
 	}
 
-	return 0;
+	return res;
 }
 
-int module_stop() {
-	return 0;
+// enable type=2 screenshot encoding
+tai_hook_ref_t shell_encode_screenshot_ref;
+int shell_encode_screenshot_patch(void **ss_arg1, unsigned unk) {
+	// set format to 2 = "raw" but we will hook and change that later
+
+	if(get_system_screenshot_format() != 0){ // for devkit jpg
+		((int*)(*ss_arg1))[8/4] = 2;
+	}
+
+	return TAI_CONTINUE(int, shell_encode_screenshot_ref, ss_arg1, unk);
+}
+
+
+// change extension from jpg to bmp
+const char bmp_path[] = "ur0:temp/screenshot/capture.bmp";
+
+// change branch for 0x34560004 (screenshot disable) to 0x34560003 (screenshot enable)
+const char mov_r3_r7[] = {
+	0x3B, 0x00, 0x00, 0x00
+};
+
+void _start() __attribute__ ((weak, alias ("module_start")));
+int module_start(SceSize argc, const void *args){
+
+	tai_module_info_t info;
+	info.size = sizeof(info);
+	if(taiGetModuleInfo("SceShell", &info) != 0){
+		return SCE_KERNEL_START_FAILED;
+	}
+
+	switch (info.module_nid) {
+	case 0x0552F692: // 3.60 Retail
+		HookOffset(info.modid, 0x247e00, 1, shell_add_watermark);
+		HookOffset(info.modid, 0x365f46, 1, shell_encode_screenshot);
+		HookOffset(info.modid, 0x36bd22, 1, shell_encode_type2);
+		taiInjectData(info.modid, 0, 0x248840, mov_r3_r7, 2);
+		taiInjectData(info.modid, 0, 0x5148b8, bmp_path, sceClibStrnlen(bmp_path, 0xFFFF) + 1);
+        break;
+	case 0xEAB89D5C: // 3.60 Testkit
+		HookOffset(info.modid, 0x240234, 1, shell_add_watermark);
+		HookOffset(info.modid, 0x35c98e, 1, shell_encode_screenshot);
+		HookOffset(info.modid, 0x36276a, 1, shell_encode_type2);
+		taiInjectData(info.modid, 0, 0x240C74, mov_r3_r7, 2);
+		taiInjectData(info.modid, 0, 0x508B18, bmp_path, sceClibStrnlen(bmp_path, 0xFFFF) + 1);
+        break;
+	case 0x6CB01295: // 3.60 Devkit
+		HookOffset(info.modid, 0x23b8a8, 1, shell_add_watermark);
+		HookOffset(info.modid, 0x35809a, 1, shell_encode_screenshot);
+		HookOffset(info.modid, 0x35de76, 1, shell_encode_type2);
+        break;
+	case 0x5549BF1F: // 3.65 Retail
+	case 0x34B4D82E: // 3.67 Retail
+	case 0x12DAC0F3: // 3.68 Retail
+		HookOffset(info.modid, 0x247e9c, 1, shell_add_watermark);
+		HookOffset(info.modid, 0x36638a, 1, shell_encode_screenshot);
+		HookOffset(info.modid, 0x36c166, 1, shell_encode_type2);
+		taiInjectData(info.modid, 0, 0x2488dc, mov_r3_r7, 2);
+		taiInjectData(info.modid, 0, 0x514df8, bmp_path, sceClibStrnlen(bmp_path, 0xFFFF) + 1);
+        break;
+
+	default:
+		sceClibPrintf("unknown SceShell, NID:0x%08X\n", info.module_nid);
+		return SCE_KERNEL_START_FAILED;
+        break;
+
+	}
+
+	return SCE_KERNEL_START_SUCCESS;
+}
+
+int module_stop(SceSize argc, const void *args) {
+	return SCE_KERNEL_STOP_SUCCESS;
 }
